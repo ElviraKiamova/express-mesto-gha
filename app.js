50c5d1c9b56d60c4a712aa1fd1759f265ded7d2d
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-09-24 16:57:20 +0500

    fix: поиск ошибок

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  // useUnifiedTopology: true,
   extended: true,
 }));
 
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
-
 app.use((req, res, next) => {
   req.user = {
-    _id: '622458d61f7bc162137cd177',
+    _id: '5d8b8592978f8bd833ca8133',
   };
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.listen(PORT);
