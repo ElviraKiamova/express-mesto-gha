1c6ac567b4b349756432a8af614de590c11da9c5
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-09-30 22:27:47 +0500

    fix: исправить схему пользователя

--- models/user.js
+++ models/user.js
@@ -7,19 +7,16 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
     maxlength: 30,
-    default: 'Эльвира',
   },
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
-    default: 'ммм',
   },
   avatar: {
     type: String,
     required: true,
-    default: 'https://adonius.club/uploads/posts/2022-06/1654146798_19-adonius-club-p-milenkie-kotiki-krasivo-foto-21.jpg',
   },
 });
 
