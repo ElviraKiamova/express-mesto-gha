a978fdf7fc1fbc94addafba406161d4176341983
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-10-14 14:05:39 +0500

    fix: править роуты

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUser,
@@ -8,10 +9,36 @@ const {
   getUserMe,
 } = require('../controllers/users');
 
+function validate(url) {
+  const regex = /^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/g;
+  if (regex.test(url)) {
+    return url;
+  }
+  throw new Error('Невалидная ссылка');
+}
+
+module.exports = { validate };
+
 router.get('/', getUser);
-router.get('/:userId', getUserId);
-router.patch('/me', updateUserInfo);
-router.patch('/me/avatar', updateAvatar);
-router.patch('/me/avatar', updateAvatar);
 router.get('/me', getUserMe);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUserInfo);
+
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().length(24).hex().required(),
+  }),
+}), getUserId);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().custom(validate),
+  }),
+}), updateAvatar);
+
 module.exports = router;
