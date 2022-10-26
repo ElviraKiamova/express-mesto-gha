9b6efb5e5280d984640d8733c9e3ccd550e23d81
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-10-26 19:12:34 +0500

    fix: править auth под токен

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,25 @@
 const jwt = require('jsonwebtoken');
 const NotAuthorized = require('../errors/NotAuthorized');
 
+const extractBearerToken = (header) => {
+  header.replace('Bearer ', '');
+};
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new NotAuthorized('Необходима авторизация'));
   }
-  const token = String(req.headers.authorization).replace('Bearer ', '');
+  // const token = String(req.headers.authorization).replace('Bearer ', '');
+
+  const token = extractBearerToken(authorization);
 
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    // payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
     return next(new NotAuthorized('Необходима авторизация'));
   }
