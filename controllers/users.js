df8cca85912c8228da178a4efbcc986d04fc65f4
Kiamova Elvira <ela.kiyamova@yandex.ru>
2022-10-27 10:07:23 +0500

    fix: исправить авторизацию и аутентификацию

--- controllers/users.js
+++ controllers/users.js
@@ -63,12 +63,14 @@ module.exports.createUser = (req, res, next) => {
       email,
       password: hash,
     }))
-    .then((user) => {
+    .then(() => {
       res.status(200).send({
-        name: user.name,
-        about: user.about,
-        avatar: user.avatar,
-        email: user.email,
+        data: {
+          name,
+          about,
+          avatar,
+          email,
+        },
       });
     })
     .catch((err) => {
@@ -126,34 +128,14 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: true,
-      });
-      res.status(200).send({ message: 'Авторизация успешна', token });
+      const token = jwt.sign(
+        { _id: user._id },
+        'super-strong-secret',
+        { expiresIn: '7d' },
+      );
+      res.send({ token });
     })
-    .catch((err) => {
-      if (err.message === 'IncorrectEmail') {
-        next(new NotAuthorized('Не правильный логин или пароль'));
-      }
-      next(err);
+    .catch(() => {
+      next(new NotAuthorized('Не правильный логин или пароль'));
     });
 };
-
-// module.exports.login = (req, res, next) => {
-//   const { email, password } = req.body;
-//   return User.findUserByCredentials(email, password)
-//     .then((user) => {
-//       res.send({
-//         token: jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' }),
-//       });
-//     })
-//     .catch((err) => {
-//       if (err.message === 'IncorrectEmail') {
-//         next(new NotAuthorized('Не правильный логин или пароль'));
-//       }
-//       next(err);
-//     });
-// };
